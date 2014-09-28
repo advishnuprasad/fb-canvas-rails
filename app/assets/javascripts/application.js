15aecd40e62d5ae315db712b48ba2728628a55c8
advishnuprasad <advishnuprasad@gmail.com>
2014-09-28 11:29:09 +0530

    Adding document ready

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,28 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function(){
+  FB.getLoginStatus(function(response) {
+        if (response.status == 'connected') {
+            var user_id = response.authResponse.userID;
+            var page_id = "330123823834776"; //coca cola
+            // var the_query = FB.Data.query(fql_query);
+
+            FB.api('/me/likes/330123823834776', function(response) {
+            // the_query.wait(function(rows) {
+
+                if(response.data.length){
+                  //the user already likes your page 
+                  alert("Liked");
+                }
+                else{
+                  //the user doesn'y like your page, or the user hasn't granted permission for you to access his likes. show him the like button
+                  alert("Not Liked");
+                  }
+            });
+        } else {
+            alert("Not Logged in");
+        }
+    });
+});
